04671db3b38205d7d060eef40ddb63a7e5cbece5
testMyName <testName@gmail.com>
2014-10-29 12:38:53 +0200

    Minor changes.

--- lesson-2/hw/ParseMyCSVFile.php
+++ lesson-2/hw/ParseMyCSVFile.php
@@ -1,50 +1,54 @@
 <?php
 
-class ParseMyCSVFile {
+class ParseMyCSVFile
+{
 
-	public 	$getFileName, $dataToSTDOUT, $delimiterSimbol, $counter, $fileHandler, $dataFromFile;
+    public $getFileName, $dataToSTDOUT, $delimiterSimbol, $counter, $fileHandler, $dataFromFile;
 
-	public function __construct($_getFileName) {
-		$this->getFileName = $_getFileName;
-		$this->dataToSTDOUT = "";
-		$this->delimiterSimbol = ",";
-		$this->counter = 0;
-		$this->fileHandler;
-		$this->dataFromFile;
-	}
+    public function __construct($_getFileName)
+    {
+        $this->getFileName = $_getFileName;
+        $this->dataToSTDOUT = "";
+        $this->delimiterSimbol = ",";
+        $this->counter = 0;
+        $this->fileHandler;
+        $this->dataFromFile;
+    }
 
-	public function PrintToConsole() {
-		fwrite(STDOUT, "\n");
-		return fwrite(STDOUT, $this->dataToSTDOUT) . PHP_EOL;
-	}
+    public function PrintToConsole()
+    {
+        fwrite(STDOUT, "\n");
+        return fwrite(STDOUT, $this->dataToSTDOUT) . PHP_EOL;
+    }
 
-	public function GetFile(){
-		$this->fileHandler = fopen($this->getFileName, "r");
-		return $this->fileHandler;
-	}
+    public function GetFile()
+    {
+        $this->fileHandler = fopen($this->getFileName, "r");
+        return $this->fileHandler;
+    }
 
-	public function GetCSVFile(){
-		$this->dataFromFile = fgetcsv($this->fileHandler, $this->delimiterSimbol);
-		return $this->dataFromFile;
-	}	
+    public function GetCSVFile()
+    {
+        $this->dataFromFile = fgetcsv($this->fileHandler, $this->delimiterSimbol);
+        return $this->dataFromFile;
+    }
 
-	public function ParseFile(){
-		if ($this->GetFile() !== FALSE) {
-		    while ($this->GetCSVFile() !== FALSE) {
+    public function ParseFile()
+    {
+        if ($this->GetFile() !== false) {
+            while ($this->GetCSVFile() !== false) {
 
-		        for ($this->counter=0; $this->counter < count($this->dataFromFile); $this->counter++) {
-		            $this->dataToSTDOUT = $this->dataToSTDOUT . $this->dataFromFile[$this->counter] . ' | ';
-		        }
+                for ($this->counter = 0; $this->counter < count($this->dataFromFile); $this->counter++) {
+                    $this->dataToSTDOUT = $this->dataToSTDOUT . $this->dataFromFile[$this->counter] . ' | ';
+                }
 
-		        $this->PrintToConsole();
-		        $this->dataToSTDOUT = "";
-		    }
-		    fclose($this->fileHandler);
-		}
-	}
+                $this->PrintToConsole();
+                $this->dataToSTDOUT = "";
+            }
+            fclose($this->fileHandler);
+        }
+    }
 }
 
 $dt = new ParseMyCSVFile("csv_file.csv");
 echo $dt->ParseFile() . PHP_EOL;
-
-?>
